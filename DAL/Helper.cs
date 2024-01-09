bed49a6fed6ad6d3b7bc546de6374100790b69c3
Kaan <115550337+kaansyn@users.noreply.github.com>
2024-01-09 10:39:56 +0300

    Güncellendi

---
+++ DAL/Helper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public class HelperException : Exception
+{
+    public HelperException(string message) : base(message)
+    {
+    }
+
+    public HelperException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
+
+public class Helper : IDisposable
+{
+    private SqlConnection cn;
+    private SqlCommand cmd;
+    private string cstr = ConfigurationManager.ConnectionStrings["cstr"].ConnectionString;
+
+    public int ExecuteNonQuery(string cmdtext, SqlParameter[] p = null)
+    {
+        try
+        {
+            using (cn = new SqlConnection(cstr))
+            {
+                using (cmd = new SqlCommand(cmdtext, cn))
+                {
+                    if (p != null)
+                    {
+                        cmd.Parameters.AddRange(p);
+                    }
+                    cn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new HelperException("Helper sınıfında bir hata oluştu.", ex);
+        }
+    }
+
+    public SqlDataReader ExecuteReader(string cmdtext, SqlParameter[] p = null)
+    {
+        try
+        {
+            cn = new SqlConnection(cstr);
+            cmd = new SqlCommand(cmdtext, cn);
+            if (p != null)
+            {
+                cmd.Parameters.AddRange(p);
+            }
+            cn.Open();
+            return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+        catch (Exception ex)
+        {
+            throw new HelperException("Helper sınıfında bir hata oluştu.", ex);
+        }
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            if (cn != null && cn.State != ConnectionState.Closed)
+            {
+                cn.Close();
+                cn.Dispose();
+                cmd.Dispose();
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new HelperException("Helper sınıfında bir hata oluştu.", ex);
+        }
+    }
+}
---
+++ Gazi.OkulAppSube2BLG/frmOgrKayit.cs
@@ -0,0 +1,125 @@
+﻿using OkulApp.BLL;
+using OkulApp.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gazi.OkulAppSube2BLG
+{
+    public partial class frmOgrKayit : Form
+    {
+        public int Ogrenciid { get; set; }
+
+        public void TextEmpty(bool kontrol)
+        {
+            if (kontrol)
+            {
+                txtAd.Text = string.Empty;
+                txtSoyad.Text = string.Empty;
+                txtNumara.Text = string.Empty;
+                btnSil.Visible = false;
+                btnGuncelle.Visible = false;
+                MessageBox.Show("İşlem başarılı");
+            }
+            else
+            {
+                MessageBox.Show("İşlem başarısız");
+            }
+        }
+
+       
+        public frmOgrKayit()
+        {
+            InitializeComponent();
+        }
+
+
+        //Dispose
+        //Garbage Collector
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var obl = new OgrenciBL();
+                bool sonuc = obl.OgrenciEkle(new Ogrenci { Ad = txtAd.Text.Trim(), Soyad = txtSoyad.Text.Trim(), Numara = txtNumara.Text.Trim() });
+                TextEmpty(sonuc);
+            }
+            catch (SqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 2627:
+                        MessageBox.Show("Bu numara daha önce kayıtlı");
+                        break;
+                    default:
+                        MessageBox.Show("Veritabanı Hatası!");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bir hata oluştu!!");
+            }
+        
+
+
+    }
+
+
+
+    private void btnBul_Click(object sender, EventArgs e)
+        {
+            var frm = new frmOgrBul(this);
+            frm.Show();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            var obl = new OgrenciBL();
+            TextEmpty(obl.OgrenciSil(Ogrenciid));
+            
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            var obl = new OgrenciBL();
+            TextEmpty(obl.OgrenciGuncelle(new Ogrenci { Ad = txtAd.Text.Trim(), Soyad = txtSoyad.Text.Trim(), Numara = txtNumara.Text.Trim(), Ogrenciid = Ogrenciid }));
+            
+        }
+    }
+
+    //interface ITransfer
+    //{
+    //    int EFT(string aliciiban, string gondereniban, double tutar);
+    //    int Havale(string aliciiban, string gondereniban, double tutar, DateTime tarih);
+    //}
+
+    //class TransferIslemleri : ITransfer
+    //{
+    //    public int EFT(string aliciiban, string gondereniban, double tutar)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public int Havale(string aliciiban, string gondereniban, double tutar, DateTime tarih)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+   // }
+}
+
+//n Katmanlı Mimari
+
+//Öğrenci bulunma durumuna göre Sil ve Güncelle Butonları Aktifliği   // Kolaydı 
+//Textbox'ların text'lerinin temizlenmesi  //  Zorlanmadım
+//Öğrenci bulunduğunda bul formunun kapanması // Eforsuz    
+//Try-Catch'ler Katmanlar arası exception yönetimi // 
+//Dispose Pattern - IDisposeble Interface
+//Singleton Design Pattern
